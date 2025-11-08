dad1c1169221deb377141ffbc198eafe13d7621b
Levente Hujer <lehujber@gmail.com>
2025-11-08 14:55:42 +0200

    feat: initial infra setup

---
+++ answer-service/src/config.py
@@ -0,0 +1,15 @@
+import os
+import logging
+
+LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO").upper()
+
+logging.basicConfig(level=logging.INFO)
+baseLogger = logging.getLogger("answer-service")
+baseLogger.setLevel(LOG_LEVEL)
+
+def get_logger(name: str) -> logging.Logger:
+    return baseLogger.getChild(name)
+
+NATS_HOST = os.getenv("NATS_HOST", "nats-server")
+NATS_PORT = int(os.getenv("NATS_PORT", 4222))
+NATS_ANSWER_TOPIC = os.getenv("NATS_ANSWER_TOPIC", "answer")
---
+++ answer-service/src/main.py
@@ -0,0 +1,35 @@
+import asyncio
+from nats.aio.client import Client as NATS
+from nats.aio.msg import Msg as NATSMsg
+
+from .config import (
+    NATS_HOST,
+    NATS_PORT,
+    NATS_ANSWER_TOPIC as topic,
+    get_logger
+)
+
+logger = get_logger("main")
+
+async def message_handler(msg: NATSMsg):
+    data = msg.data.decode()
+    logger.debug(f"Received a message on '{msg.subject}': {data}")
+
+    # TODO: implement answer generation
+    await msg.respond(b"Sample answer response")
+
+async def main():
+    logger.info("Starting Answer Service...")
+
+
+    nc = NATS()
+    await nc.connect(f"nats://{NATS_HOST}:{NATS_PORT}")
+    await nc.subscribe(topic, cb=message_handler)
+
+    logger.debug(f"Subscribed to topic '{topic}' on NATS server at {NATS_HOST}:{NATS_PORT}")
+
+    while True:
+        await asyncio.sleep(1)
+
+if __name__ == "__main__":
+    asyncio.run(main())
